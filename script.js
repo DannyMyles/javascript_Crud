295585b1497b692de5bb64201010ee7873b1f974
DannyMyles <dannymyles1999@gmail.com>
2022-03-10 13:41:17 +0300

    added edit and delete function

--- script.js
+++ script.js
@@ -14,23 +14,23 @@ function onFormSubmit() {
 function readFormData() {
     var formData = {};
     formData["fullName"] = document.getElementById("fullName").value;
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["salary"] = document.getElementById("salary").value;
-    formData["city"] = document.getElementById("city").value;
+    formData["stack"] = document.getElementById("stack").value;
+    formData["mc"] = document.getElementById("mc").value;
+    formData["tm"] = document.getElementById("tm").value;
     return formData;
 }
 
 function insertNewRecord(data) {
-    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var table = document.getElementById("studentList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     cell1 = newRow.insertCell(0);
     cell1.innerHTML = data.fullName;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.empCode;
+    cell2.innerHTML = data.stack;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
+    cell3.innerHTML = data.mc;
     cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
+    cell4.innerHTML = data.tm;
     cell4 = newRow.insertCell(4);
     cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
@@ -38,30 +38,30 @@ function insertNewRecord(data) {
 
 function resetForm() {
     document.getElementById("fullName").value = "";
-    document.getElementById("empCode").value = "";
-    document.getElementById("salary").value = "";
-    document.getElementById("city").value = "";
+    document.getElementById("stack").value = "";
+    document.getElementById("mc").value = "";
+    document.getElementById("tm").value = "";
     selectedRow = null;
 }
 
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
     document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("stack").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("mc").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("tm").value = selectedRow.cells[3].innerHTML;
 }
 function updateRecord(formData) {
     selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.empCode;
-    selectedRow.cells[2].innerHTML = formData.salary;
-    selectedRow.cells[3].innerHTML = formData.city;
+    selectedRow.cells[1].innerHTML = formData.stack;
+    selectedRow.cells[2].innerHTML = formData.mc;
+    selectedRow.cells[3].innerHTML = formData.tm;
 }
 
 function onDelete(td) {
     if (confirm('Are you sure to delete this record ?')) {
         row = td.parentElement.parentElement;
-        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        document.getElementById("studentList").deleteRow(row.rowIndex);
         resetForm();
     }
 }
